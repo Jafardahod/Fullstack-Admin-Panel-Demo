e43866fecda76356afc68ca4b148b954d6e71153
jafardahod <jafardahod7@gmail.com>
2025-11-27 00:27:21 +0530

    fixed http issue

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
   // baseURL: 'http://localhost:5000/api',
-  baseURL: 'http://https://fullstack-admin-panel-demo.vercel.app/api',
+  baseURL: 'https://fullstack-admin-panel-demo.vercel.app/api',
 });
 
 axiosClient.interceptors.request.use((config) => {
