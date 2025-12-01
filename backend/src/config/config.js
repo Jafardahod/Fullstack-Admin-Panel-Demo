869c5d0a08a52e048ce9da67eb15631871141bc0
jafardahod <jafardahod7@gmail.com>
2025-12-01 23:36:38 +0530

    fixed some issues

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,2 +1,3 @@
 export const JWT_SECRET = process.env.JWT_SECRET || 'supersecretjwtkey';
 export const JWT_EXPIRES_IN = '1h';
+console.log(JWT_SECRET)
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,40 @@
+// backend/src/config/db.js
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_PORT,
+  DB_SSL, // 'true' or 'false'
+} = process.env;
+
+console.log("🔌 MySQL Config:");
+console.log("DB_HOST:", DB_HOST);
+console.log("DB_PORT:", DB_PORT);
+console.log("DB_USER:", DB_USER);
+console.log("DB_NAME:", DB_NAME);
+console.log("DB_SSL:", DB_SSL);
+
+const ssl =
+  DB_SSL === "true"
+    ? { rejectUnauthorized: false } // Required for Railway
+    : undefined;
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'assignment_app',
+  host: DB_HOST || "localhost",
+  user: DB_USER || "root",
+  password: DB_PASSWORD || "",
+  database: DB_NAME || "assignment_app",
+  port: DB_PORT ? Number(DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl,
 });
 
 export default pool;
